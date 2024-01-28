d84ebf54f30a027a0077b01437945a491fcca080
Andrew Bessonov <andrew.bessonov.ru@gmail.com>
2024-01-28 23:18:23 +0300

    Поправил ссылку

--- src/main/java/ru/bessik/price/controller/PriceController.java
+++ src/main/java/ru/bessik/price/controller/PriceController.java
@@ -71,7 +71,7 @@ public class PriceController {
      *
      * @return статус
      */
-    @PostMapping("/update-all/{telegramId}")
+    @PostMapping("/prices/update-all/{telegramId}")
     public StatusResponse updateAllForUser(@PathVariable String telegramId) {
         log.info("[API] update all prices for user {}", telegramId);
         productService.updateAllForUser(telegramId);
